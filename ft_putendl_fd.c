51aec21fb92f07a212ea74d75bf2d77f7788a709
François L. GIRARD <82939484+francoisgirard51@users.noreply.github.com>
2022-07-13 17:32:57 +0200

    Update ft_putendl_fd.c

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -3,9 +3,9 @@ Librairie :           Non-standard
 Role de la fonction : Sort la chaîne vers un fichier donné avec une nouvelle
                       ligne
 Commentaires :        Ecrit la chaine s sur le descripteur de fichier fd
-					  suivi d'un '\n'
+		      suivi d'un '\n'
 Parametres :          1) s : chaîne de caractères à écrire
-					  2) fd : descripteur de fichier
+		      2) fd : descripteur de fichier
 Retourne :            N/A
 ---------------------------------------------------------------------------- */
 
