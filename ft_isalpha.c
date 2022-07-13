b9a143edb52f446e2c8060a24bca19ceb2289e04
François L. GIRARD <82939484+francoisgirard51@users.noreply.github.com>
2022-07-13 17:34:57 +0200

    Update ft_isalpha.c

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -2,7 +2,7 @@
 Librairie:           <ctype.h>
 Role de la fonction: test de caractère alphabétique.
 Commentaires :       Verifie si le caractère est alphabétique, équivalent à
-					 ft_isupper ou ft_islower
+		     ft_isupper ou ft_islower
 Parametres :         1) c : un caractère à tester
 Retourne :           1 si le caractère est alphabétique, 0 sinon
 ---------------------------------------------------------------------------- */
